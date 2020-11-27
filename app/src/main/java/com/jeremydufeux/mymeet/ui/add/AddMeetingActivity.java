3c90373d65dc6203402c0d053a34b9025bc6c365
Jérémy Dufeux <jeremy.dufeux@gmail.com>
2020-11-27 18:53:56 +0100

    Work on AddMeetingActivity

--- app/src/main/java/com/jeremydufeux/mymeet/ui/add/AddMeetingActivity.java
+++ app/src/main/java/com/jeremydufeux/mymeet/ui/add/AddMeetingActivity.java
@@ -1,37 +1,79 @@
 package com.jeremydufeux.mymeet.ui.add;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.DatePickerDialog;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.widget.DatePicker;
 
-import com.jeremydufeux.mymeet.R;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+
+import com.jeremydufeux.mymeet.databinding.ActivityAddMeetingBinding;
 import com.jeremydufeux.mymeet.di.DI;
+import com.jeremydufeux.mymeet.dialog.DatePickerFragment;
 import com.jeremydufeux.mymeet.model.Meeting;
+import com.jeremydufeux.mymeet.service.MeetingApiService;
 
-import java.util.List;
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
-public class AddMeetingActivity extends AppCompatActivity {
-
+public class AddMeetingActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener{
     public static final String BUNDLE_EXTRA_MEETING_ID = "BUNDLE_EXTRA_MEETING_ID";
+    private MeetingApiService mService;
+    private ActivityAddMeetingBinding mBinding;
+    private Meeting mMeeting;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_meeting);
+        mBinding = ActivityAddMeetingBinding.inflate(getLayoutInflater());
+        View view = mBinding.getRoot();
+        setContentView(view);
+
+        mService = DI.getMeetingApiService();
 
         if(getIntent().getExtras() != null && getIntent().getExtras().containsKey(BUNDLE_EXTRA_MEETING_ID)) {
-            Log.d("ContentValues", "Edit mode");
             long id = getIntent().getExtras().getLong(BUNDLE_EXTRA_MEETING_ID);
-            List<Meeting> meetings = DI.getMeetingApiService().getMeetings();
-            for (Meeting meeting : meetings) {
+
+            for (Meeting meeting : mService.getMeetings()) {
                 if (meeting.getId() == id) {
-                    Log.d("ContentValues", "Edit meeting : " + meeting.getSubject());
+                    mMeeting = meeting;
+                    break;
                 }
             }
+            loadData();
         }else{
-            Log.d("ContentValues", "Add mode");
+            loadHint();
         }
+    }
+
+    private void loadData(){
+        mBinding.addMeetingDateEt.setText(getDate());
+        mBinding.addMeetingDateEt.setOnClickListener(v -> {
+            DialogFragment datePicker = new DatePickerFragment(mMeeting.getDate());
+            datePicker.show(getSupportFragmentManager(), "date picker");
+        });
+    }
+    private void loadHint() {
+        mBinding.addMeetingDateEt.setHint(getDate());
+    }
+
+
+
+    @Override
+    public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.YEAR, year);
+        cal.set(Calendar.MONTH, month);
+        cal.set(Calendar.DAY_OF_MONTH, day);
+        String date = DateFormat.getDateInstance().format(cal.getTime());
+        mBinding.addMeetingDateEt.setText(date);
+    }
+
+    private String getDate(){
+        Calendar cal = Calendar.getInstance();
+        return String.format(Locale.FRANCE, "%d/%d/%d", cal.get(Calendar.DAY_OF_MONTH), cal.get(Calendar.MONTH), cal.get(Calendar.YEAR));
     }
 
 }
