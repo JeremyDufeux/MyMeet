fe399d4e08c0c9b7584f4446805e76876550eef9
Jérémy Dufeux <jeremy.dufeux@gmail.com>
2020-12-08 13:08:26 +0100

    Use clear with calendar

--- app/src/main/java/com/jeremydufeux/mymeet/utils/Tools.java
+++ app/src/main/java/com/jeremydufeux/mymeet/utils/Tools.java
@@ -20,26 +20,21 @@ public class Tools {
     }
 
     public static Calendar getCalendarFromDate(int year, int month, int day, int hour, int minute){ // get calendar a from year, month, day, hour and minute
-        Calendar cal = Calendar.getInstance();  // TODO Check clear()
+        Calendar cal = Calendar.getInstance();
+        cal.clear();
         cal.set(Calendar.YEAR, year);
         cal.set(Calendar.MONTH, month);
         cal.set(Calendar.DAY_OF_MONTH, day);
         cal.set(Calendar.HOUR_OF_DAY, hour);
         cal.set(Calendar.MINUTE, minute);
-        cal.set(Calendar.SECOND, 0);
-        cal.set(Calendar.MILLISECOND, 0);
         return cal;
     }
 
     public static Calendar getCalendarFromTime(int hour, int minute){ // get calendar a from hour and minute
         Calendar cal = Calendar.getInstance();
-        cal.set(Calendar.YEAR, 0);
-        cal.set(Calendar.MONTH, 0);
-        cal.set(Calendar.DAY_OF_MONTH, 0);
+        cal.clear();
         cal.set(Calendar.HOUR_OF_DAY, hour);
         cal.set(Calendar.MINUTE, minute);
-        cal.set(Calendar.SECOND, 0);
-        cal.set(Calendar.MILLISECOND, 0);
         return cal;
     }
 
